304652caca002f3a7dd833a4289069dcc7eb5bfa
Alexey V. Smirnov (CHUWI) <l--182@yandex.ru>
2026-07-17 09:13:54 +0300

    create leads table and model

---
+++ www/app/Models/Lead.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Lead extends Model
+{
+    use HasFactory;
+    use Notifiable;
+
+    /**
+     * The table associated with the model.
+     */
+    protected $table = 'leads';
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'comment',
+    ];
+
+    /**
+     * Get the email address for notifications.
+     * Используется для отправки уведомлений самому лиду
+     */
+    public function routeNotificationForMail(): string
+    {
+        return $this->email;
+    }
+}
