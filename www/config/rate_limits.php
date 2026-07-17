dcacc50fd7740c484fb52f5f96d0859425b2ab47
Alexey V. Smirnov (CHUWI) <l--182@yandex.ru>
2026-07-17 13:07:09 +0300

    set throttle control on api/contact route

---
+++ www/config/rate_limits.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'contact' => [
+        'per_minute' => env('RATE_LIMIT_CONTACT_PER_MINUTE', 1),
+    ],
+];
--- www/routes/api.php
+++ www/routes/api.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\Api\LeadController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/contact', [LeadController::class, 'store'])->name('api.contact.store');
+Route::post('/contact', [LeadController::class, 'store'])->name('api.contact.store')
+            ->middleware('throttle:' . config('rate_limits.contact.per_minute').',1');
